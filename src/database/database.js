6ccf4fd9464048a5b72666c3d80fcfcde15c184f
RenatoUbuntu <renatoyferreira@gmail.com>
2023-05-12 10:10:05 -0300

    basic settings

---
+++ src/database/database.js
@@ -0,0 +1,15 @@
+import pg from "pg"
+import dotenv from "dotenv"
+dotenv.config()
+
+const { Pool } = pg
+
+const configDatabase = {
+    connectionString: process.env.DATABASE_URL
+}
+
+// const connection = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//   });
+
+const db = new Pool(configDatabase)
