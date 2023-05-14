bbd10918ee847c81602bc9230c187fd6986263bd
RenatoUbuntu <renatoyferreira@gmail.com>
2023-05-14 19:54:22 -0300

    create: getCustomersById

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -14,3 +14,34 @@ export async function createNewCustomer(req, res){
         res.status(500).send(err.message)
     }
 }
+
+export async function getCustomers(req, res) {
+    try {
+      const { rows } = await db.query(
+        `
+          SELECT * FROM customers;
+          `
+      );
+      res.status(200).send(rows);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+}
+
+export async function getCustomersById(req, res){
+    const { id } = req.params
+
+    try{
+        const customer = await db.query(
+
+            `SELECT * FROM customers WHERE id= $1;`,
+            [id]
+        
+        )
+            if(customer.rows.length === 0) return res.sendStatus(404)
+
+            res.status(200).send(customer.rows[0])
+    }catch(err){
+        res.status(500).send(err.message);
+    }
+}
--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -1,11 +1,12 @@
 import express from "express"
 import { validSchemaCustomers } from "../middlewares/customers.middlewares.js"
-import { createNewCustomer } from "../controllers/customers.controllers.js"
+import { createNewCustomer, getCustomers, getCustomersById } from "../controllers/customers.controllers.js"
 
 const router = express.Router()
 
 router.post("/customers", validSchemaCustomers, createNewCustomer)
-// router.get("/customers")
+router.get("/customers", getCustomers)
+router.get("/customers/:id", getCustomersById)
 // router.put("/customers")
 
 
