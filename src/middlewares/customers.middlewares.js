76ee95accaa51a6452e41086c26ff8cb9dd7c426
RenatoUbuntu <renatoyferreira@gmail.com>
2023-05-15 12:00:59 -0300

    for available

--- src/middlewares/customers.middlewares.js
+++ src/middlewares/customers.middlewares.js
@@ -24,15 +24,14 @@ export async function validSchemaCustomersEdit(req, res, next){
     const customer = req.body
     const { id } = req.params
 
-    const isAnotherUser = await db.query("SELECT * FROM customers WHERE cpf = $1 AND id <> $2;", [customer.cpf, id]);
-    if (isAnotherUser.rows.length > 0) return res.sendStatus(409);
-
     const validation = customerSchema.validate(customer, { abortEarly: false})
-
     if(validation.error){
         const errors = validation.error.details.map((detail) => detail.message)
         return res.status(400).send(errors)
     }
+
+    const isAnotherUser = await db.query("SELECT * FROM customers WHERE cpf = $1 AND id <> $2;", [customer.cpf, id]);
+    if (isAnotherUser.rows.length > 0) return res.sendStatus(409);
 
     res.locals.customer = customer 
     next()
